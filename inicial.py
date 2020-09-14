39ff8581f63b67c585e9af5d34621246c1004f2b
A01720572 <71277782+A01720572@users.noreply.github.com>
2020-09-14 11:35:02 -0500

    Update inicial.py

--- inicial.py
+++ inicial.py
@@ -1,12 +1,14 @@
-#matricula y nombre 1
+#A01720572 Luciana Montalvo Barragán
 #matricula y nombre 2
-#proposito del programa
+#Calcular el área y perímetro de un rectángulo dado sus medidas de los lados
 
 #funcion1  calcular área del rectángulo
 
 
 #funcion2  calcular perímetro del rectángulo
-
+def perimetro (l1,l2):
+    p = (l1*2) + (l2*2)
+    return p
 
 #instrucciones de accion
 #pedir datos
@@ -19,3 +21,5 @@ l2 = float(input())
 #desplegar calculo funcion1
 
 #desplegar calculo funcion 2
+perimetro = perimetro(l1,l2)
+print(perimetro)
