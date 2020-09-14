a1fb07714ec2643a45e04f4845b331a2de42caf1
melissamtzm <a00827008@itesm.mx>
2020-09-14 11:41:49 -0500

    Update inicial.py

--- inicial.py
+++ inicial.py
@@ -1,8 +1,10 @@
 #A01720572 Luciana Montalvo Barragán
-#matricula y nombre 2
+#A00827008 Melissa Martínez Mendoza
 #Calcular el área y perímetro de un rectángulo dado sus medidas de los lados
 
 #funcion1  calcular área del rectángulo
+def  area(l1,l2):
+    return l1 * l2
 
 
 #funcion2  calcular perímetro del rectángulo
@@ -19,7 +21,11 @@ print("medida de un lado del rectángulo")
 l2 = float(input())
 
 #desplegar calculo funcion1
+area = area(l1,l2)
+print("el area del rectángulo es:")
+print(area)
 
 #desplegar calculo funcion 2
 perimetro = perimetro(l1,l2)
+print("el perímetro del rectángulo es:")
 print(perimetro)
